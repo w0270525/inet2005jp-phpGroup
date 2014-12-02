cddda459b1f8de0901427cb17b57213085e0fdc9
K Wallace <W0247248@nscc.ca>
2014-12-02 16:13:56 +0000

    Formatting, tried to add the functions.php but it broke everything...

--- view/admin/articleviews/displayArticlesView.php
+++ view/admin/articleviews/displayArticlesView.php
@@ -1,15 +1,13 @@
-
 <style type="text/css">
-    table
-    {
-        border: 1px solid purple;
-    }
-    th, td
-    {
-        border: 1px solid red;
-    }
+  table
+  {
+    border: 1px solid purple;
+  }
+  th, td
+  {
+    border: 1px solid red;
+  }
 </style>
-
 <?php
   if(!empty($lastOperationResults)):
 ?>
@@ -18,48 +16,47 @@
   endif;
 ?>
 <h1>Site Articles</h1>
-
 <table class="table">
-    <thead>
+  <thead>
     <tr>
-        <th>Name</th>
-        <th>Title</th>
-        <th>Content Area</th>
-        <th>Content</th>
-        <th>Associated Page</th>
-        <th>All Pages</th>
-        <th>Blurb</th>
-        <th>Description</th>
-        <th>Created By</th>
-        <th>Created Date</th>
-        <th>Modified By</th>
-        <th>Modified Date</th>
-        <th>Edit</th>
+      <th>Name</th>
+      <th>Title</th>
+      <th>Content Area</th>
+      <th>Content</th>
+      <th>Associated Page</th>
+      <th>All Pages</th>
+      <th>Blurb</th>
+      <th>Description</th>
+      <th>Created By</th>
+      <th>Created Date</th>
+      <th>Modified By</th>
+      <th>Modified Date</th>
+      <th>Edit</th>
     </tr>
-    </thead>
-    <tbody>
-    <?php
-      foreach($arrayOfArticles as $article):
-    ?>
-        <tr>
-            <span id='articleId'><?php echo $article->getId(); ?></span>
-            <td><?php echo $article->getName(); ?></td>
-            <td><?php echo $article->getTitle(); ?></td>
-            <td><?php echo $article->getContentarea(); ?></td>
-            <td><?php echo $article->getContent(); ?></td>
-            <td><?php echo $article->getAssocPage(); ?></td>
-            <td><?php echo $article->getAllPages(); ?></td>
-            <td><?php echo $article->getBlurb(); ?></td>
-            <td><?php echo $article->getDesc(); ?></td>
-            <td><?php echo $article->getCreatedBy(); ?></td>
-            <td><?php echo $article->getCreatedDate(); ?></td>
-            <td><?php echo $article->getModifiedBy(); ?></td>
-            <td><?php echo $article->getModifiedDate(); ?></td>
-            <td><a href="?pageupdate=<?php echo $article->getId() ; ?>"><span class="glyphicon glyphicon-pencil" ></span></a></td>
-        </tr>
-    <?php
-      endforeach;
-    ?>
-    </tbody>
-    <tfoot></tfoot>
+  </thead>
+  <tbody>
+  <?php
+    foreach($arrayOfArticles as $article):
+  ?>
+    <tr>
+      <span id='articleId'><?php echo $article->getId(); ?></span>
+      <td><?php echo $article->getName(); ?></td>
+      <td><?php echo $article->getTitle(); ?></td>
+      <td><?php echo $article->getContentarea(); ?></td>
+      <td><?php echo $article->getContent(); ?></td>
+      <td><?php echo $article->getAssocPage(); ?></td>
+      <td><?php echo $article->getAllPages(); ?></td>
+      <td><?php echo $article->getBlurb(); ?></td>
+      <td><?php echo $article->getDesc(); ?></td>
+      <td><?php echo $article->getCreatedBy(); ?></td>
+      <td><?php echo $article->getCreatedDate(); ?></td>
+      <td><?php echo $article->getModifiedBy(); ?></td>
+      <td><?php echo $article->getModifiedDate(); ?></td>
+      <td><a href="?pageupdate=<?php echo $article->getId() ; ?>"><span class="glyphicon glyphicon-pencil" ></span></a></td>
+    </tr>
+  <?php
+    endforeach;
+  ?>
+  </tbody>
+  <tfoot></tfoot>
 </table>
