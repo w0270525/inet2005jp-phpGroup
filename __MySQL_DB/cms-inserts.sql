14b793873a6d9cdf2edd2e809f4c7e91550108d3
“Aaron <aaronali780@gmail.com>
2014-12-02 14:53:40 +0000

    ** UPDATED DBASE SCRIPT ** contains triggers for some table modified and created values You will get errors regarding deafult colums and such  if you dont update the dbase with the new script

--- __MySQL_DB/cms-inserts.sql
+++ __MySQL_DB/cms-inserts.sql
@@ -19,28 +19,30 @@ CREATE TABLE IF NOT EXISTS `cms`.`USER` (
   `u_fname` VARCHAR(45) NOT NULL,
   `u_lname` VARCHAR(45) NOT NULL,
   `u_username` VARCHAR(45) NOT NULL,
-  `u_pass` VARCHAR(128) NOT NULL,
-  `u_salt` VARCHAR(32) NOT NULL,
+  `u_pass` VARCHAR(512) NOT NULL,
+  `u_salt` VARCHAR(512) NOT NULL,
   `u_createddate` DATETIME NOT NULL ,
   `u_createdby` INT(11) NOT NULL,
   `u_modifieddate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
   `u_lastmodifiedby` INT(11) NULL DEFAULT NULL,
+  `u_key` VARCHAR(512) NULL DEFAULT NULL,
   PRIMARY KEY (`u_id`),
   CONSTRAINT `u_createdby`
-    FOREIGN KEY (`u_createdby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`u_createdby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `u_lastmodifiedby`
-    FOREIGN KEY (`u_lastmodifiedby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`u_lastmodifiedby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `u_id_UNIQUE` ON `cms`.`USER` (`u_id` ASC);
+CREATE UNIQUE INDEX `u_username_UNIQUE` ON `cms`.`USER` (`u_username` ASC);
 
 CREATE INDEX `u_createdby_idx` ON `cms`.`USER` (`u_createdby` ASC);
 
@@ -61,20 +63,21 @@ CREATE TABLE IF NOT EXISTS `cms`.`PAGES` (
   `p_creationdate` DATETIME NOT NULL ,
   `p_lastmodifiedby` INT(11) NULL DEFAULT NULL,
   `p_modifieddate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
+  `p_style` TEXT NULL DEFAULT NULL ,
   PRIMARY KEY (`p_id`),
   CONSTRAINT `p_createdby`
-    FOREIGN KEY (`p_createdby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`p_createdby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `p_lastmodifiedby`
-    FOREIGN KEY (`p_lastmodifiedby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`p_lastmodifiedby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `p_id_UNIQUE` ON `cms`.`PAGES` (`p_id` ASC);
 
@@ -85,6 +88,9 @@ CREATE UNIQUE INDEX `p_alias_UNIQUE` ON `cms`.`PAGES` (`p_alias` ASC);
 CREATE INDEX `p_createdby_idx` ON `cms`.`PAGES` (`p_createdby` ASC);
 
 CREATE INDEX `p_lastmodifiedby_idx` ON `cms`.`PAGES` (`p_lastmodifiedby` ASC);
+CREATE TRIGGER PAGES_ADD_TRIGGER BEFORE INSERT ON PAGES FOR EACH ROW   SET  NEW.p_creationdate = NOW(),NEW.p_modifieddate= NOW();
+CREATE TRIGGER PAGES_UPDATE_TRIGGER BEFORE UPDATE ON PAGES FOR EACH ROW   SET   NEW.p_modifieddate= NOW();
+
 
 
 -- -----------------------------------------------------
@@ -104,18 +110,18 @@ CREATE TABLE IF NOT EXISTS `cms`.`CONTENT_AREAS` (
   `c_a_modifieddate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
   PRIMARY KEY (`c_a_id`),
   CONSTRAINT `c_a_createdby`
-    FOREIGN KEY (`c_a_createdby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`c_a_createdby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `c_a_lastmodifiedby`
-    FOREIGN KEY (`c_a_lastmodifiedby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`c_a_lastmodifiedby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 7
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 7
+  DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `h_id_UNIQUE` ON `cms`.`CONTENT_AREAS` (`c_a_id` ASC);
 
@@ -128,6 +134,8 @@ CREATE UNIQUE INDEX `h_desc_UNIQUE` ON `cms`.`CONTENT_AREAS` (`c_a_desc` ASC);
 CREATE INDEX `h_createdby_idx` ON `cms`.`CONTENT_AREAS` (`c_a_createdby` ASC);
 
 CREATE INDEX `h_lastmodifiedby_idx` ON `cms`.`CONTENT_AREAS` (`c_a_lastmodifiedby` ASC);
+CREATE TRIGGER CONTENT_AREA_INSERT_TRIGGER BEFORE INSERT ON CONTENT_AREAS FOR EACH ROW   SET   nEW.c_a_creationdate = NOW(),NEW.c_a_modifieddate= NOW();
+CREATE TRIGGER CONTENT_UPDATE_INSERT_TRIGGER BEFORE UPDATE ON CONTENT_AREAS FOR EACH ROW   SET   NEW.c_a_modifieddate= NOW();
 
 
 -- -----------------------------------------------------
@@ -151,28 +159,28 @@ CREATE TABLE IF NOT EXISTS `cms`.`ARTICLE` (
   `a_allpages` TINYINT(1) NULL DEFAULT '0',
   PRIMARY KEY (`a_id`),
   CONSTRAINT `a_assocpage`
-    FOREIGN KEY (`a_assocpage`)
-    REFERENCES `cms`.`PAGES` (`p_id`)
+  FOREIGN KEY (`a_assocpage`)
+  REFERENCES `cms`.`PAGES` (`p_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `a_contentarea`
-    FOREIGN KEY (`a_contentarea`)
-    REFERENCES `cms`.`CONTENT_AREAS` (`c_a_id`)
+  FOREIGN KEY (`a_contentarea`)
+  REFERENCES `cms`.`CONTENT_AREAS` (`c_a_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `a_createdby`
-    FOREIGN KEY (`a_createdby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`a_createdby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `a_lastmodifiedby`
-    FOREIGN KEY (`a_lastmodifiedby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`a_lastmodifiedby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 7
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 7
+  DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `a_id_UNIQUE` ON `cms`.`ARTICLE` (`a_id` ASC);
 
@@ -189,6 +197,9 @@ CREATE INDEX `a_createdby_idx` ON `cms`.`ARTICLE` (`a_createdby` ASC);
 CREATE INDEX `a_lastmodifiedby_idx` ON `cms`.`ARTICLE` (`a_lastmodifiedby` ASC);
 
 CREATE INDEX `a_c_a_id_idx` ON `cms`.`ARTICLE` (`a_contentarea` ASC);
+CREATE TRIGGER ARTICLE_INSERT_TRIGGER BEFORE INSERT ON ARTICLE FOR EACH ROW   SET   nEW.a_creationdate = NOW(),NEW.a_modifieddate= NOW();
+CREATE TRIGGER ARTICLE_UPDATE_INSERT_TRIGGER BEFORE UPDATE ON ARTICLE FOR EACH ROW   SET   NEW.a_modifieddate= NOW();
+
 
 
 -- -----------------------------------------------------
@@ -200,9 +211,9 @@ CREATE TABLE IF NOT EXISTS `cms`.`LOOKUP_ROLES` (
   `l_r_id` INT(11) NOT NULL AUTO_INCREMENT,
   `l_r_name` VARCHAR(6) NOT NULL,
   PRIMARY KEY (`l_r_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 4
+  DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `l_ed_id_UNIQUE` ON `cms`.`LOOKUP_ROLES` (`l_r_id` ASC);
 
@@ -224,18 +235,18 @@ CREATE TABLE IF NOT EXISTS `cms`.`STYLE` (
   `s_modifieddate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
   PRIMARY KEY (`s_id`),
   CONSTRAINT `s_createdby`
-    FOREIGN KEY (`s_createdby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`s_createdby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `s_lastmodifiedby`
-    FOREIGN KEY (`s_lastmodifiedby`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`s_lastmodifiedby`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `s_id_UNIQUE` ON `cms`.`STYLE` (`s_id` ASC);
 
@@ -259,18 +270,18 @@ CREATE TABLE IF NOT EXISTS `cms`.`USER_ROLES` (
   `u_r_l_r_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`u_r_id`),
   CONSTRAINT `u_r_l_d`
-    FOREIGN KEY (`u_r_l_r_id`)
-    REFERENCES `cms`.`LOOKUP_ROLES` (`l_r_id`)
+  FOREIGN KEY (`u_r_l_r_id`)
+  REFERENCES `cms`.`LOOKUP_ROLES` (`l_r_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `u_r_u_id`
-    FOREIGN KEY (`u_r_u_id`)
-    REFERENCES `cms`.`USER` (`u_id`)
+  FOREIGN KEY (`u_r_u_id`)
+  REFERENCES `cms`.`USER` (`u_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 4
+  DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `u_r_id_UNIQUE` ON `cms`.`USER_ROLES` (`u_r_id` ASC);
 
@@ -291,11 +302,11 @@ INSERT INTO cms.LOOKUP_ROLES VALUES (3, 'author');
 
 # u_id, u_fname, u_lname, u_username, u_pass, u_salt, u_createdby
 INSERT INTO cms.USER (u_id, u_fname, u_lname, u_username, u_pass, u_salt, u_createdby,u_createddate)
- VALUES (1,'admin', 'User', 'admin', 'admin', 'salt', 1,NOW());
+VALUES (1,'admin', 'User', 'admin', 'admin', 'salt', 1,NOW());
 INSERT INTO cms.USER (u_id,u_fname, u_lname, u_username, u_pass, u_salt, u_createdby,u_createddate)
- VALUES (2,'editor', 'User', 'editor', 'admin', 'pepper', 1,NOW());
+VALUES (2,'editor', 'User', 'editor', 'admin', 'pepper', 1,NOW());
 INSERT INTO cms.USER (u_id,u_fname, u_lname, u_username, u_pass, u_salt, u_createdby,u_createddate)
- VALUES (3,'author', 'User', 'author', 'admin', 'sugar', 1,NOW());
+VALUES (3,'author', 'User', 'author', 'admin', 'sugar', 1,NOW());
 
 # u_r_id, u_r_u_id, u_r_l_r_id
 INSERT INTO cms.USER_ROLES VALUES (1, 1, 1);
@@ -304,34 +315,41 @@ INSERT INTO cms.USER_ROLES VALUES (3, 1, 3);
 
 # s_id, s_name, s_desc, s_style, s_active, s_createdby
 INSERT INTO cms.STYLE (s_id, s_name, s_desc, s_style, s_active, s_createdby,s_creationdate)
- VALUES (1, 'Default', 'Default Style Selection', 'body { display: block }', 1, 1,NOW());
+VALUES (1, 'Default', 'Default Style Selection', 'body { display: block }', 1, 1,NOW());
 INSERT INTO cms.STYLE (s_id, s_name, s_desc, s_style, s_active, s_createdby,s_creationdate)
- VALUES (2, 'Inverse', 'Colors inverted Style Selection.', 'body { background-color: black; text-color: white }', 0, 1,NOW());
+VALUES (2, 'Inverse', 'Colors inverted Style Selection.', 'body { background-color: black; text-color: white }', 0, 1,NOW());
 
 # p_id, p_name, p_alias, p_desc, p_style, p_createdby
 INSERT INTO cms.PAGES (p_id, p_name, p_alias, p_desc, p_createdby,p_creationdate)
-		VALUES (1, 'INDEX', 'index', 'This is the main page for all your content', 1,NOW());
+VALUES (1, 'INDEX', 'index', 'This is the main page for all your content', 1,NOW());
 INSERT INTO cms.PAGES (p_id, p_name, p_alias, p_desc, p_createdby,p_creationdate)
-		VALUES (2, 'NEWS', 'news', 'This page is forn all your news and updates', 1,NOW());
+VALUES (2, 'NEWS', 'news', 'This page is forn all your news and updates', 1,NOW());
 
 # c_a_id, c_a_name, c_a_alias, c_a_desc, c_a_order, c_a_createdby
 INSERT INTO cms.CONTENT_AREAS (c_a_id, c_a_name, c_a_alias, c_a_desc, c_a_order, c_a_createdby,c_a_creationdate)
- VALUES (1, 'Header', 'header', 'Header content area', 1, 1,NOW());
+VALUES (1, 'Header', 'header', 'Header content area', 1, 1,NOW());
 INSERT INTO cms.CONTENT_AREAS (c_a_id, c_a_name, c_a_alias, c_a_desc, c_a_order, c_a_createdby,c_a_creationdate)
- VALUES (2, 'Article', 'article', 'Article content area', 2, 1,NOW());
+VALUES (2, 'Article', 'article', 'Article content area', 2, 1,NOW());
 INSERT INTO cms.CONTENT_AREAS (c_a_id, c_a_name, c_a_alias, c_a_desc, c_a_order, c_a_createdby,c_a_creationdate)
- VALUES (3, 'Footer', 'footer', 'Footer content area', 3, 1,NOW());
+VALUES (3, 'Footer', 'footer', 'Footer content area', 3, 1,NOW());
 
 # a_id, a_contentarea, a_name, a_title, a_content, a_assocpage, a_createdby
 INSERT INTO cms.ARTICLE (a_id, a_contentarea, a_name, a_title, a_content, a_assocpage, a_createdby,a_creationdate)
- VALUES (1, 1, 'header1_test', 'Test1', '<h1>Test Header</h1>', 1, 1,NOW());
+VALUES (1, 1, 'header1_test', 'Test Tile Artilc1', 'New content areas can be added to the front-end site from the administrative back-end and they will be stored in the CMS database with their name, alias (used for the DIV ids that will tie into CSS directives), order on the page (e.g. a header content area should show up first on the page), and description all stored appropriately. In addition, fields for created by indicator, creation date, modify by indicator, and modify date will be maintained in accordance with Security requirement: REQ-0018. ', 1, 1,NOW());
 INSERT INTO cms.ARTICLE (a_id, a_contentarea, a_name, a_title, a_content, a_assocpage, a_createdby,a_creationdate)
- VALUES (2, 1, 'header2_test', 'Test2', '<h1>Test Headerr</h1>', 2, 1,NOW());
+VALUES (2, 1, 'header2_test', 'Artic trst', '<h1>Test Headerr</h1>New content areas can be added to the front-end site from the administrative back-end and they will be stored in the CMS database with their name, alias (used for the DIV ids that will tie into CSS directives), order on the page (e.g. a header content area should show up first on the page), and description all stored appropriately. In addition, fields for created by indicator, creation date, modify by indicator, and modify date will be maintained in accordance with Security requirement: REQ-0018. ', 2, 1,NOW());
 INSERT INTO cms.ARTICLE (a_id, a_contentarea, a_name, a_title, a_content, a_assocpage, a_createdby,a_creationdate)
- VALUES (3, 2, 'article1_test', 'Test3', '<h1>Test Article</h1>', 1, 1,NOW());
+VALUES (3, 2, 'article1_test', 'MVC WHAT', '<h1>Test Article</h1>', 1, 1,NOW());
 INSERT INTO cms.ARTICLE (a_id, a_contentarea, a_name, a_title, a_content, a_assocpage, a_createdby,a_creationdate)
- VALUES (4, 2, 'article2_test', 'Test4', '<h1>Test Articler</h1>', 2, 1,NOW());
+VALUES (4, 2, 'article2_test', 'Fantatsc', '
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 2, 1,NOW());
 INSERT INTO cms.ARTICLE (a_id, a_contentarea, a_name, a_title, a_content, a_assocpage, a_createdby,a_creationdate)
- VALUES (5, 3, 'footer1_test', 'Test5', '<h1>Test Footer</h1>', 1, 1,NOW());
+VALUES (5, 3, 'footer1_test', 'Test5 i think', '<h1>Test Footer</h1>fsdfsdfsdfdsfsdfsdfsdfsdfsdfsdfsdf', 1, 1,NOW());
 INSERT INTO cms.ARTICLE (a_id, a_contentarea, a_name, a_title, a_content, a_assocpage, a_createdby,a_creationdate)
- VALUES (6, 3, 'footer2_test', 'Test6', '<h1>Test Footerr</h1>', 2, 1,NOW());
+VALUES (6, 3, 'footer2_test', 'lupa ipsm', '"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using -Content here, content here-, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+', 2, 1,NOW());
