a5e3f39c6a049990ff37ca5d11f7d03e08ddc5a1
K Wallace <W0247248@nscc.ca>
2014-12-02 20:03:45 +0000

    No clue what I'm really doing but I cleaned up some excess code and formatting here.

--- view/admin/pageviews/addPageView.php
+++ view/admin/pageviews/addPageView.php
@@ -11,56 +11,43 @@
 </style>
 
 <?php
-if(!empty($lastOperationResults)):
-    ?>
-    <h2><?php echo $lastOperationResults; ?></h2>
+  if(!empty($lastOperationResults)):
+?>
+<h2><?php echo $lastOperationResults; ?></h2>
 <?php
-endif;
+  endif;
 ?>
 <h1>Add  A New  Page</h1>
 <table class="table">
-    <thead>
+  <thead>
     <tr>
-
-        <th>Name</th>
-        <th>Alias</th>
-        <th>Description</th>
-
+      <th>Name</th>
+      <th>Alias</th>
+      <th>Description</th>
     </tr>
-    </thead>
-    <form name="addNewPageForm"  id="addNewPageForm" class="addNewPageForm" onclick ="$('#addPageSubmit').hide();$('#verifyf').show()"
-        action="#" method="post" value="addNewPageForm">
+  </thead>
+  <form name="addNewPageForm"  id="addNewPageForm" class="addNewPageForm" onclick ="$('#addPageSubmit').hide();$('#verifyf').show()"
+      action="#" method="post" value="addNewPageForm">
     <tbody>
-
-
-
-        <tr>
-
-            <td><input oninput="resetBut()" type="text" name = "p_name" required /></td>
-            <td><input oninput="resetBut()" type="text" name = "p_alias" required/></td>
-            <td><input oninput="resetBut()" type="text" name = "p_desc" required/></td>
-            <input type="hidden" name = "formSubmitNewPage" value="true" required/>
-            <td><span  class="btn btn-default" id="formConfirm" onclick="verifyf()" >Verify </span>
-             <input type="submit" class="btn btn-default" id="addPageSubmit" onclick="verifyf();resetBut(); " value ="Confirm" >  </td>
-
-        </tr>
-        <tr>
-
-            <td></td>
-        </tr>
-
-
-
-
-    </tbody></form>
-    <tfoot></tfoot>
+      <tr>
+        <td><input oninput="resetBut()" type="text" name ="p_name" required /></td>
+        <td><input oninput="resetBut()" type="text" name ="p_alias" required /></td>
+        <td><input oninput="resetBut()" type="text" name ="p_desc" required /></td>
+        <input type="hidden" name = "formSubmitNewPage" value="true" required />
+        <td><span class="btn btn-default" id="formConfirm" onclick="verifyf()" >Verify</span>
+        <input type="submit" class="btn btn-default" id="addPageSubmit" onclick="verifyf();resetBut();" value ="Confirm" /></td>
+      </tr>
+    </tbody>
+  </form>
+  <tfoot></tfoot>
 </table>
+
 <script>
     function  verifyf()
     {
 //        if (document.forms['addNewPageForm']['p_name'].value.length>5)
-//      if   (document.forms['addNewPageForm']['p_alias'].value.length>5)
-//        if( document.forms['addNewPageForm']['p_desc'].value.length>20)
+//        if (document.forms['addNewPageForm']['p_alias'].value.length>5)
+//        if (document.forms['addNewPageForm']['p_desc'].value.length>20)
 //    {
         $('#formConfirm').hide();
         $('#addPageSubmit').show();
