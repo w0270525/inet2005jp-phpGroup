af5f6525db446084ba6667a999c0e8cba0af1a81
“Aaron <aaronali780@gmail.com>
2014-12-03 17:24:21 +0000

    fixing add content form with some JS verification

--- controller/ContentAreaController.php
+++ controller/ContentAreaController.php
@@ -65,8 +65,8 @@ class ContentAreaController
 
             if(  $lastOperationResults=$this->model->addContentArea(new ContentArea(null,$_POST["c_name"],$_POST["c_alias"],$_POST["c_desc"],null,null,null,$user->getId(),null,$user->getId(),null)))
             {
-                $arrayOfPages[0] = $this->model->getContentAreaByName($_POST["p_name"]);
-                include'../view/admin/pageviews/editPageViews.php';
+                $arrayOfPages[0] = $this->model->getContentAreaByName($_POST["c_name"]);
+                include'../view/admin/contentviews/editContentViews.php';
             }
         }
 
