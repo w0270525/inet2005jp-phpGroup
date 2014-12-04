4074ba97cad54f515f6bfb571a0a124f2426263c
K Wallace <W0247248@nscc.ca>
2014-12-04 23:16:21 +0000

    Moved the current page construction to the model to thin out the controller.

--- controller/PageController.php
+++ controller/PageController.php
@@ -79,44 +79,8 @@ class PageController
 
       // Nav Array;
       $navArray = $this->model->getAllPages();
-
       // Create the current page;
-      $currentPage = $this->model->getPage($id);
-      // Current Style;
-      $styleControl = new StyleModel();
-      $currentStyle = $styleControl->getActiveStyle();
-      $currentPage->setStyle($currentStyle);
-
-      // Get an array of all articles which belong to this page or appear on all pages;
-      $articleControl = new ArticleModel();
-      $arrayOfArticles = $articleControl->getAllArticlesByPageId($id);
-
-      // Create an array of all the Content Areas;
-      $contentAreaControl = new ContentAreaModel();
-      $arrayOfContentAreas = $contentAreaControl->getAllContentAreas();
-
-      // Loop through content areas and add articles associated with that area to
-      // that areas article array using the array_push function.
-      foreach ($arrayOfContentAreas as $ca) {
-
-        $tempArray = array();
-
-        foreach ($arrayOfArticles as $a) {
-
-          if ($a->getContentarea() == $ca->getId()) {
-
-            array_push($tempArray, $a);
-
-          } // if END
-        } // foreach (a) END
-
-        $ca->setArticles($tempArray);
-
-      } // foreach (ca) END
-
-      // Set the content area array to the current page;
-      $currentPage->setContentAreas($arrayOfContentAreas);
-
+      $currentPage = $this->model->constructDisplayPage($id);
       // show the website
       include '../view/displayPage.php';
 
