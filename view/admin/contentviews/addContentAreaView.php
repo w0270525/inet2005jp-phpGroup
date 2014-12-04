ec0ebfdd9be9290c38976ca860ffec488b957046
Jonathan Prince <w0270525@nscc.ca>
2014-12-03 16:20:52 -0800

    added functionality to check for whitespace in alias as per document

--- view/admin/contentviews/addContentAreaView.php
+++ view/admin/contentviews/addContentAreaView.php
@@ -51,7 +51,7 @@ endif;
     function  verifyAddArticle()
     {
         if (document.forms['addNewContentArea']['c_name'].value.length>5)
-            if ((/\s/.test(s)), document.forms['addNewContentArea']['c_alias'] )
+	        if ((/\s/.test(s)), document.forms['addNewContentArea']['c_alias'] )
              if(document.forms['addNewContentArea']['c_alias'].value.length>5)
                 if( document.forms['addNewContentArea']['c_desc'].value.length>20)
         {
