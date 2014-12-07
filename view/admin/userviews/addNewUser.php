0f3d930a475161749bc775c520d3fe3f0a89fce9
“Aaron <aaronali780@gmail.com>
2014-12-07 19:25:47 +0000

    Addded new Functions.pho - leaving  it for the last think to do to ease with testing

--- view/admin/userviews/addNewUser.php
+++ view/admin/userviews/addNewUser.php
@@ -1,14 +1,10 @@
 
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-<head>
+<button type="button" class="btn btn-success" data-toggle="collapse" data-target="#aduSerbutt_">Add A New User</button>
+<div id="aduSerbutt_" class="collapse in">
 
-    <title>Edit User</title>
-    <script src="functions.js"></script>
+
+ 
+  <!--   <script src="functions.js"></script>1-->
 
     <style type="text/css">
         .glyphicon
@@ -27,8 +23,7 @@ and open the template in the editor.
             border: 1px solid red;
         }
     </style>
-</head>
-<body>
+ 
 <?php
 
 require_once('functions.php');
@@ -44,57 +39,59 @@ if(verifyAdmin())
 
 
     ?>
-    <h1>ADD A NEW USER</h1>
-    <table  class="table table-striped table-hover " data-toggle="table" id="table-style" data-row-style="rowStyle">
-        <thead>
-        <tr>
-            <th>Username</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-
-            <th>Admin</th>
-            <th>Editor</th>
-            <th>Author</th>
-            <th>Edit</th>
-        </tr>
-        </thead><form onsubmit="return formSubmitted()" method="post" name="addNewUser" id="addNewUser" >
-            <tbody>
 
 
 
-            <tr><input class="hidden" type="hidden" name="bnasd3432er" id="bnasd3432er" value=<?php echo $bnasd3432er?>  >
-
-                <td><input type="text" name="userName" id="userName"     required  pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$"   /></td>
-                <td><input type="text" name="FirstName" id="FirstName"    required pattern="^[a-zA-Z][a-zA-Z]{3,20}$"   /></td>
-                <td><input type="text" name="LastName" id="LastName"   required pattern="^[a-zA-Z][a-zA-Z]{3,20}$"  /></td>
-                <td><input type="radio" name="admin" id="admin"   />  </td>
-                <td><input type="radio" name="editor" id="editor" />  </td>
-                <td><input type="radio" name="author" id="author" />   </td>
-                <td> <a class="button"  value="addUser" id="unhideSubmit"  onclick="$('#formSubmit').show();" >Add User</a></td>
-
-            </tr>
-            <tr id="formSubmit" class="formSubmit" >
-                <td colspan="10">
-                    <div style="text-align:center;">
-                        Please Confirm A NEW USER <input type="submit" value="addUser" id="addUser" class="button pull-right"  onclick="$('#formSubmit').hide() "/>
-
-                    </div>
-                </td>
-            </tr>
+             <form onsubmit="return verifyformusername()" method="post" name="addNewUser" id="addNewUser" >
 
 
 
 
+             <input class="hidden" type="hidden" name="bnasd3432er" id="bnasd3432er" value=<?php echo $bnasd3432er?>  >
 
-            </tbody></form>
-        <tfoot></tfoot>
-    </table>
+                <label>UserName</label><input type="text" name="userName" id="userName"       pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$"   />
+                <label>First Name</label><input type="text" name="FirstName" id="FirstName"     pattern="^[a-zA-Z][a-zA-Z]{3,20}$"   />
+                <label>Last Name</label><input type="text" name="LastName" id="LastName"    pattern="^[a-zA-Z][a-zA-Z]{3,20}$"  />
+                <label>Admin</label><input type="checkbox" name="admin" id="admin"   />
+                <label>Editor</label><input type="checkbox" name="editor" id="editor" />
+                <label>Author</label><input type="checkbox" name="author" id="author" />
+                <label>Inactive</label> <input type="checkbox" name="inanctive" id="inactive" />
+
+
+
+                <div><a class="btn btn-default form-control"  type="button" value="Verify" id="unhideSubmit"  onclick="verifyformusername()" >Add User</a>
+                 </div>
+                 <input type="submit"   value="addUser" id="addUserSubmnitButton" class="btn btn-default form-control"  onclick="verifyformusername();resetBut();"/>
+</form>
+
+ </div>
     <script>
-        $('#formSubmit').hide();
+        
+        
+
+        function  verifyformusername()
+        {
+            if (document.forms['addNewUser']['userName'].value.length>5)
+
+                if   (document.forms['addNewUser']['FirstName'].value.length>5)
+                    if (document.forms['addNewUser']['LastName'].value.length>5)
 
 
-        function formSubmitted(){
+                {
+                    $('#unhideSubmit').hide();
+                    $('#addUserSubmnitButton').show();
+                    return true;
+                }
+        return false;
+        }
 
+
+        $('#addUserSubmnitButton').hide();
+
+        function resetButto()
+        {
+            $('#unhideSubmit').show();
+            $('#addUserSubmnitButton').hide();
         }
 
 
@@ -104,5 +101,4 @@ if(verifyAdmin())
 <?php
 }// end if admin
 ?>
-</body>
-</html>
+ 
