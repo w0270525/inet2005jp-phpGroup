0f5cc2321982c3b999c89f65f4c2211b471ebe22
“Aaron <aaronali780@gmail.com>
2014-12-12 13:25:16 +0000

    got dongut chat workgin i think, having a ard time withthe bar chart though

--- view/admin/userviews/chartView.php
+++ view/admin/userviews/chartView.php
@@ -10,33 +10,51 @@
 <script>
 
 
-	var barChartData = {
-		labels : [<?php
-		foreach($arrayOfChartObjects['u_name'] as $arr) {echo '"'. $arr .'", ';}?>],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,0.8)",
-				highlightFill: "rgba(220,220,220,0.75)",
-				highlightStroke: "rgba(220,220,220,1)",
-				data : [<?php
-		foreach($arrayOfChartObjects['a_count'] as $arr) {echo '"'. $arr .'", ';}?>]
-//			},
-//			{
-//				fillColor : "rgba(151,187,205,0.5)",
-//				strokeColor : "rgba(151,187,205,0.8)",
-//				highlightFill : "rgba(151,187,205,0.75)",
-//				highlightStroke : "rgba(151,187,205,1)",
-//				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-//			}
-		]
+ //	var barChartData = {
+<!--		labels : [--><?php
+//		foreach($arrayOfChartObjects as $arr)
+//		{  echo '"'.$arr["u_name"] .'", ';}?><!--"some"],-->
+<!--		datasets : [-->
+<!--			{-->
+<!--				fillColor : "rgba(220,220,220,0.5)",-->
+<!--				strokeColor : "rgba(220,220,220,0.8)",-->
+<!--				highlightFill: "rgba(220,220,220,0.75)",-->
+<!--				highlightStroke: "rgba(220,220,220,1)",-->
+<!--				data : [--><?php
+//		foreach($arrayOfChartObjects as $arr) {echo  $arr["a_count"] .', ';}?><!--1]-->
+<!-- 			},-->
+<!--			{-->
+<!--				fillColor : "rgba(151,187,205,0.5)",-->
+<!--				strokeColor : "rgba(151,187,205,0.8)",-->
+<!--				highlightFill : "rgba(151,187,205,0.75)",-->
+<!--				highlightStroke : "rgba(151,187,205,1)",-->
+<!--				data : 2,3,4,5,6-->
+<!-- 			}-->
+<!--		]-->
+<!---->
+ //	}
 
-	}
-	window.onload = function(){
-		var ctx = document.getElementById("canvas").getContext("2d");
-		window.myBar = new Chart(ctx).Bar(barChartData, {
-			responsive : true
-		});
-	}
+        var doughnutData = [
+            <?php foreach($arrayOfChartObjects as $arr):?>
+            {
+                value: <?php echo $arr["a_count"];?>,
+                color:"#F7464A",
+                highlight: "#FF5A5E",
+                label:  "<?php echo strval($arr["u_name"]);?>"
+            },
+            <?php endforeach;?>
+
+            {
+                value: 0,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: ""
+            }
+
+        ];
+
+        var ctx = document.getElementById("canvas").getContext("2d");
+        window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+
 
 </script>
