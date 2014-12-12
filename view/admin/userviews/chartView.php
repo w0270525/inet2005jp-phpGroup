40889f782c2ed884728e20c00b178b9edb3c377b
Jonathan Prince <w0270525@nscc.ca>
2014-12-12 09:09:36 -0800

    doughnut now has random colouring

--- view/admin/userviews/chartView.php
+++ view/admin/userviews/chartView.php
@@ -6,40 +6,40 @@
 ?>
 
 <h2>User Content Frequency</h2>
-<canvas id="canvas" height="200" width="300"></canvas>
+<canvas id="canvas" height="400" width="400"></canvas>
+<!--<canvas id="canvas1" height="200" width="300"></canvas>-->
 <script>
 
 
- //	var barChartData = {
-<!--		labels : [--><?php
-//		foreach($arrayOfChartObjects as $arr)
-//		{  echo '"'.$arr["u_name"] .'", ';}?><!--"some"],-->
+<!-- var barChartData = {-->
+<!---->
+<!--		labels : [--><?php //foreach($arrayOfChartObjects as $arr){ echo $arr["u_name"];}?><!--],-->
 <!--		datasets : [-->
 <!--			{-->
 <!--				fillColor : "rgba(220,220,220,0.5)",-->
 <!--				strokeColor : "rgba(220,220,220,0.8)",-->
 <!--				highlightFill: "rgba(220,220,220,0.75)",-->
 <!--				highlightStroke: "rgba(220,220,220,1)",-->
-<!--				data : [--><?php
-//		foreach($arrayOfChartObjects as $arr) {echo  $arr["a_count"] .', ';}?><!--1]-->
+<!--				data : [--><?php //foreach($arrayOfChartObjects as $arr){ echo $arr["a_count"];}?><!--]-->
 <!-- 			},-->
-<!--			{-->
-<!--				fillColor : "rgba(151,187,205,0.5)",-->
-<!--				strokeColor : "rgba(151,187,205,0.8)",-->
-<!--				highlightFill : "rgba(151,187,205,0.75)",-->
-<!--				highlightStroke : "rgba(151,187,205,1)",-->
-<!--				data : 2,3,4,5,6-->
-<!-- 			}-->
+<!---->
+<!--	 var ctx1 = document.getElementById("canvas1").getContext("2d");-->
+<!-- window.myBar = new Chart(ctx1).Bar(barChartData, {-->
+<!--	 responsive : true-->
+<!-- });-->
 <!--		]-->
 <!---->
- //	}
+<!-- }-->
+<?php
+
+?>
 
         var doughnutData = [
             <?php foreach($arrayOfChartObjects as $arr):?>
             {
                 value: <?php echo $arr["a_count"];?>,
-                color:"#F7464A",
-                highlight: "#FF5A5E",
+                color:"#<?php $color = substr(md5(rand()), 0, 6);echo $color;?>",
+                highlight: "#5AD3D1",
                 label:  "<?php echo strval($arr["u_name"]);?>"
             },
             <?php endforeach;?>
@@ -54,7 +54,7 @@
         ];
 
         var ctx = document.getElementById("canvas").getContext("2d");
-        window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+        window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : false});
 
 
 </script>
