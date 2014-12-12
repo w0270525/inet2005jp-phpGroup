c6977ff42caa5deb70ad2da283b8d6f1c4235e15
“Aaron <aaronali780@gmail.com>
2014-12-12 12:29:27 +0000

    chart button opens chart area (naming was sligtly off)

--- _public/adm/admin.php
+++ _public/adm/admin.php
@@ -60,7 +60,7 @@ if(isset($_SESSION["logged"])  &&($_SESSION["logged"]==true))
                             <li data-target="viewUsers" id="viewUserNav" ><a href="#">View Users</a></li>
                             <li data-target="addNUser" id="addUsers"><a href="#"   >Add User</a></li>
                             <li data-target="removeUser" id="removeUsers" ><a href="#" >Remove User</a></li>
-	                        <li data-target="viewCharts" id="viewCharts" ><a href="#" >Content Charts</a></li>
+	                        <li data-target="viewCharts" id="viewChart" ><a href="#" >Content Charts</a></li>
 
                         </ul>
                     </div><!-- /btn-group -->
@@ -71,7 +71,7 @@ if(isset($_SESSION["logged"])  &&($_SESSION["logged"]==true))
                             $('#viewUsers').hide();
                             $('#addNewUser').hide();
                             $('#removeUser').hide();
-	                        $('#viewChart').hide();
+	                        $('#viewCharts').hide();
 
                         }
                     </script>
@@ -287,7 +287,7 @@ if(isset($_SESSION["logged"])  &&($_SESSION["logged"]==true))
             $tempController->userController()->addNewUser();
             ?></div>
 
-	    <div id="viewChart" class="containerAdmin">
+	    <div id="viewCharts" class="containerAdmin">
 		    <?php
 		    $tempController->chartController()->displayChart();
 		    ?>
